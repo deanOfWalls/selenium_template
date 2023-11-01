1a20b7a2d1db8ef171bfa2e4e963488c8a8f0057
deanOfWalls <deanofwalls@gmail.com>
2023-11-01 12:03:34 -0400

    commented out driver.quit() call so it's not enabled by default

--- src/test/java/com/deanofwalls/selenium_template/SeleniumTests.java
+++ src/test/java/com/deanofwalls/selenium_template/SeleniumTests.java
@@ -27,15 +27,58 @@ public class SeleniumTests {
 
         try {
 
-            //TESTING TAKES PLACE HERE
+// Navigate to the web page
+            driver.get("https://automationexercise.com/signup");
+
+            // Set an explicit wait for a specific element on the page to be visible
+            WebDriverWait wait = new WebDriverWait(driver, 10);
+            WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("button.btn:nth-child(5)")));
+            if(element.isDisplayed()){
+                System.out.println("Page appears to be loaded");
+            }
+            else{
+                System.out.println("Element wasn't found");
+            }
+
+            //input a name into the 'Name' field
+            WebElement nameField = driver.findElement(By.cssSelector(".signup-form > form:nth-child(2) > input:nth-child(2)"));
+            nameField.sendKeys("MyNameIsMud");
+
+            //input an email address into the 'Email Address' field
+            WebElement emailField = driver.findElement(By.cssSelector(".signup-form > form:nth-child(2) > input:nth-child(3)"));
+            emailField.sendKeys("primus@sucks.com");
+
+            //click the signup button
+            WebElement signupButton  =  driver.findElement(By.cssSelector("button.btn:nth-child(5)"));
+            signupButton.click();
+
+            //wait for next page to load
+            wait();
+
+            //set password
+            WebElement passwordField = driver.findElement(By.cssSelector(".signup-form > form:nth-child(2) > input:nth-child(3)"));
+            passwordField.sendKeys("Abc123IThatesME");
+
+
+            //set first name
+            WebElement firstNameField = driver.findElement(By.cssSelector(".signup-form > form:nth-child(2) > input:nth-child(3)"));
+            firstNameField.sendKeys("Aloysius");
+
+            //set last name
+            WebElement lastNameField = driver.findElement(By.cssSelector(".signup-form > form:nth-child(2) > input:nth-child(3)"));
+            lastNameField.sendKeys("Abercrombie");
+
+            //set address
+            WebElement addressField = driver.findElement(By.cssSelector(".signup-form > form:nth-child(2) > input:nth-child(3)"));
+            addressField.sendKeys("123 Trailer Park Ln");
 
         } catch (Exception e) {
             e.printStackTrace();
         }
-        finally {
-            // Close the WebDriver
-            driver.quit();
-        }
+//        finally {
+//            // Close the WebDriver
+//            driver.quit();
+//        }
 
     }
 }
